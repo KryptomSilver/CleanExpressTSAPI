dc3b9a283866a790647b5f282c967dc858f63243
KryptomSilver <abelromeror763@gmail.com>
2024-06-11 14:26:13 -0600

    feat: Add configuration for environment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 
+dotenv.config({ path: path.resolve(__dirname, "../.env") })
 const app = express()
 
 export default app
