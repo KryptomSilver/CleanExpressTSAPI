3e5c018346205af664281e4c9fa28699cc5cb289
KryptomSilver <abelromeror763@gmail.com>
2024-06-12 12:56:53 -0600

    feat: Added the route products in the main app

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import express from "express";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../.env") })
 import { checkDBMySQL } from "./database/mysql.config";
+import ProductRouter from "./product/infrastructure/route/product.router";
 
 const app = express()
-
 checkDBMySQL()
+
+app.use("/api/v1/products", ProductRouter)
+
 export default app
