b2bab66232290ad4bad46098654583d78ee5e6be
KryptomSilver <abelromeror763@gmail.com>
2024-06-12 12:59:56 -0600

    feat: Added functions of repository mysql
    In this commit, I have added functions for products repository.
    - deleteProduct
    - updateProduct
    - getAllProducts
    - getProductById

--- src/product/infrastructure/repository/product.mysql.repository.ts
+++ src/product/infrastructure/repository/product.mysql.repository.ts
@@ -3,18 +3,6 @@ import ProductEntity from "../../domain/product.entity";
 import ProductRepository from "../../domain/product.repository";
 
 class ProductMysqlRepository implements ProductRepository {
-  getAllProducts(): Promise<ProductEntity[] | null> {
-    throw new Error("Method not implemented.");
-  }
-  getProductById(idProduct: number): Promise<ProductEntity | null> {
-    throw new Error("Method not implemented.");
-  }
-  updateProduct(idProduct: number, product: ProductEntity): Promise<ProductEntity | null> {
-    throw new Error("Method not implemented.");
-  }
-  deleteProduct(idProduct: number): Promise<boolean> {
-    throw new Error("Method not implemented.");
-  }
   async creteProduct(product: ProductEntity): Promise<ProductEntity | null> {
     try {
       const [rows]: any = await pool.query(
@@ -26,7 +14,45 @@ class ProductMysqlRepository implements ProductRepository {
       console.log("ERROR MYSQL REPOSITORY", error);
       return null
     }
-  } 
+  }
+  async getAllProducts(): Promise<ProductEntity[] | null> {
+    try {
+      const [rows]: any = await pool.query(`SELECT * FROM products;`)
+      return rows
+    } catch (error: any) {
+      console.log("ERROR MYSQL REPOSITORY", error)
+      return null
+    }
+  }
+  async getProductById(idProduct: number): Promise<ProductEntity | null> {
+    try {
+      const [rows]: any = await pool.query(`SELECT * FROM products WHERE idProduct = ${idProduct};`)
+      return rows
+    } catch (error: any) {
+      console.log("ERROR MYSQL REPOSITORY", error)
+      return null
+    }
+  }
+  async updateProduct(idProduct: number, product: ProductEntity): Promise<ProductEntity | null> {
+    try {
+      const [rows]: any = await pool.query(`UPDATE products
+      SET name = '${product.name}', description = '${product.description}'
+      WHERE idProduct = ${idProduct};`)
+      return rows
+    } catch (error: any) {
+      console.log("ERROR MYSQL REPOSITORY", error)
+      return null
+    }
+  }
+  async deleteProduct(idProduct: number): Promise<boolean> {
+    try {
+      await pool.query(`DELETE FROM products WHERE idProduct = ${idProduct};`)
+      return true
+    } catch (error: any) {
+      console.log("ERROR MYSQL REPOSITORY", error)
+      return false
+    }
+  }
 }
 
 export default ProductMysqlRepository
