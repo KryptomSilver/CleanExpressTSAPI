2e023371c16e485795a361f0848c3729e199fe35
KryptomSilver <abelromeror763@gmail.com>
2024-06-12 23:03:05 -0600

    feat: Added user repository

---
+++ src/user/domain/user.repository.ts
@@ -0,0 +1,11 @@
+import UserEntity from "./user.entity";
+
+interface UserRepository {
+  createUser(user: UserEntity): Promise<UserEntity | null>
+  getAllUsers(): Promise<UserEntity[] | null>
+  getUserById(idUser: number): Promise<UserEntity | null>
+  updateUser(idUser: number): Promise<UserEntity | null>
+  deleteUser(idUser: number): Promise<boolean>
+}
+
+export default UserRepository
