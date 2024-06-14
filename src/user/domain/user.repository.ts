8e9e85e0b7ad089dcfa44f0f9d9ebc3488a908c5
KryptomSilver <abelromeror763@gmail.com>
2024-06-14 16:40:07 -0600

    fix: added user entity in repository

--- src/user/domain/user.repository.ts
+++ src/user/domain/user.repository.ts
@@ -4,7 +4,7 @@ interface UserRepository {
   createUser(user: UserEntity): Promise<UserEntity | null>
   getAllUsers(): Promise<UserEntity[] | null>
   getUserById(idUser: number): Promise<UserEntity | null>
-  updateUser(idUser: number): Promise<UserEntity | null>
+  updateUser(idUser: number, user: UserEntity): Promise<UserEntity | null>
   deleteUser(idUser: number): Promise<boolean>
 }
 
