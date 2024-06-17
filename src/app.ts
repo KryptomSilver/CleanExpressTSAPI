9614b92bf674bd21e39b437c0e3b38343db6c43d
KryptomSilver <abelromeror763@gmail.com>
2024-06-17 12:58:05 -0600

    feat: added import in app for authentication
    In this commit, I have added import for router authentication in the
    main app.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") })
 import { checkDBMySQL } from "./database/mysql.config";
 import ProductRouter from "./product/infrastructure/route/product.router";
 import UserRouter from "./user/infrastructure/route/user.router";
+import AuthenticationRouter from "./authentication/infrastructure/router/authentication.router";
 import bodyParser from "body-parser";
 
 const app = express()
@@ -13,5 +14,6 @@ checkDBMySQL()
 app.use(bodyParser.json())
 app.use("/api/v1/products", ProductRouter)
 app.use("/api/v1/users", UserRouter)
+app.use("/api/v1/authentication", AuthenticationRouter)
 
 export default app
